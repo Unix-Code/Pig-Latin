32f30b63ae1ad5406a32d052f6035c4fa85daa14
Unix-Code <dmalakh@comcast.net>
2015-12-06 00:55:09 -0500

    [W.I.P.] Pig Latin Converter

---
+++ Pig Latin/PigLatin.java
@@ -0,0 +1,40 @@
+public class PigLatin {
+    public static String convert(String start) {
+        StringBuilder sb = new StringBuilder();
+        
+        String prefix = (findFirstVowel(start) != -1) ? start.substring(0, findFirstVowel(start)) : "";
+        if (findFirstVowel(start) == 0) {
+            prefix = "w";
+        }
+        else if ("quQu".indexOf(start.substring(0,2)) != -1) {
+            prefix = "qu";
+        }
+        
+        
+        for (int i = 0; i < start.length(); i++) {
+            System.out.println(sb.toString());
+            if (i == findFirstVowel(start)) {
+                sb.delete(0, sb.length());
+            }
+            if (prefix.equals("qu")) {
+                sb.delete(0, 1);
+            }
+            sb.append(start.charAt(i));
+        }
+        sb.append(prefix + "ay");
+        System.out.println("Prefix: " + prefix);
+        return sb.toString();
+    }
+    
+    public static int findFirstVowel(String start) {
+        int position = -1;
+        for (int i = 0; i < start.length(); i++) {
+            char compare = start.charAt(i);
+            if ("aAeEiIoOuU".indexOf(compare) != -1) {
+                position = i;
+            }
+        }
+        
+        return position;
+    }
+}
