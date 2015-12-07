b429bc97b6288782c1f88c80a3739e0b12c8cc9e
Unix-Code <dmalakh@comcast.net>
2015-12-07 18:10:13 -0500

    Fixed Pig Latin Converter

--- PigLatin.java
+++ PigLatin.java
@@ -2,27 +2,29 @@ public class PigLatin {
     public static String convert(String start) {
         StringBuilder sb = new StringBuilder();
         
-        String prefix = (findFirstVowel(start) != -1) ? start.substring(0, findFirstVowel(start)) : "";
-        if (findFirstVowel(start) == 0) {
+        String prefix = (findFirstVowel(start) != -1) ? ((findFirstVowel(start) == 0) ? "w" : (("quQu".indexOf(start.substring(0,2)) != -1) ? "qu" : start.substring(0, findFirstVowel(start)))) : "";
+        /*if (findFirstVowel(start) == 0) {
             prefix = "w";
         }
         else if ("quQu".indexOf(start.substring(0,2)) != -1) {
             prefix = "qu";
-        }
+        }*/
         
         
         for (int i = 0; i < start.length(); i++) {
-            System.out.println(sb.toString());
+            //System.out.println(sb.toString());
             if (i == findFirstVowel(start)) {
                 sb.delete(0, sb.length());
             }
-            if (prefix.equals("qu")) {
-                sb.delete(0, 1);
-            }
             sb.append(start.charAt(i));
         }
+        
+        if (prefix.equals("qu")) {
+            sb.delete(0, 1);
+        }
+        
         sb.append(prefix + "ay");
-        System.out.println("Prefix: " + prefix);
+        //System.out.println("Prefix: " + prefix);
         return sb.toString();
     }
     
@@ -32,6 +34,7 @@ public class PigLatin {
             char compare = start.charAt(i);
             if ("aAeEiIoOuU".indexOf(compare) != -1) {
                 position = i;
+                break;
             }
         }
         
