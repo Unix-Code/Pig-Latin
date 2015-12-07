3465709b3574c3cac807defeffbce82b6a89ba95
Unix-Code <dmalakh@comcast.net>
2015-12-07 18:57:32 -0500

    Deleted Diagnostic Prints

--- PigLatin.java
+++ PigLatin.java
@@ -1,33 +1,25 @@
 public class PigLatin {
     public static String convert(String start) {
         StringBuilder sb = new StringBuilder();
-        
+
         String prefix = (findFirstVowel(start) != -1) ? ((findFirstVowel(start) == 0) ? "w" : (("quQu".indexOf(start.substring(0,2)) != -1) ? "qu" : start.substring(0, findFirstVowel(start)))) : "";
-        /*if (findFirstVowel(start) == 0) {
-            prefix = "w";
-        }
-        else if ("quQu".indexOf(start.substring(0,2)) != -1) {
-            prefix = "qu";
-        }*/
-        
-        
-        for (int i = 0; i < start.length(); i++) {
-            //System.out.println(sb.toString());
+
+        for (int i = 0; i < start.length(); i++) {	
             if (i == findFirstVowel(start)) {
                 sb.delete(0, sb.length());
             }
             sb.append(start.charAt(i));
         }
-        
+
         if (prefix.equals("qu")) {
             sb.delete(0, 1);
         }
-        
+
         sb.append(prefix + "ay");
-        //System.out.println("Prefix: " + prefix);
+
         return sb.toString();
     }
-    
+
     public static int findFirstVowel(String start) {
         int position = -1;
         for (int i = 0; i < start.length(); i++) {
@@ -37,7 +29,7 @@ public class PigLatin {
                 break;
             }
         }
-        
+
         return position;
     }
 }
