fe3ddd62ee0f56844121ae80479e1e7fd2f29ee4
Unix-Code <dmalakh@comcast.net>
2015-12-17 17:24:53 -0500

    Added Punctuation + Full Sentence Support

--- PigLatin.java
+++ PigLatin.java
@@ -1,9 +1,35 @@
 public class PigLatin {
+    public static void main(String[] args) {
+        System.out.println(convert("My List: Bob..."));
+    }    
+
     public static String convert(String start) {
+        PigLatin pL = new PigLatin();
+        StringBuilder end = new StringBuilder();
+        String[] words = start.split("[ ]");
+
+        for (String word : words) {
+            end.append(pL.translate(word) + " ");
+        }
+
+        return end.toString();
+    }
+
+    public static String translate(String sStart) {
+        String start = sStart;
         StringBuilder sb = new StringBuilder();
+        String prefix;
 
-        String prefix = (findFirstVowel(start) != -1) ? ((findFirstVowel(start) == 0) ? "y" : (("quQu".contains(start.substring(0,2))) ? "qu" : start.substring(0, findFirstVowel(start)))) : "";
+        StringBuilder endPunctuation = new StringBuilder();
+        int itt = start.length() - 1;
+        while (Character.isLetterOrDigit(start.charAt(itt)) == false) {
+            endPunctuation.append(start.charAt(itt));
+            start = start.substring(0, start.length() - 1);
+            itt--;
+        }
+        endPunctuation.reverse();
 
+        prefix = (findFirstVowel(start) != -1) ? ((findFirstVowel(start) == 0) ? "y" : (("quQu".contains(start.substring(0,2))) ? "qu" : start.substring(0, findFirstVowel(start)))) : "";
         for (int i = 0; i < start.length(); i++) {  
             if (i == findFirstVowel(start)) {
                 sb.delete(0, sb.length());
@@ -14,8 +40,12 @@ public class PigLatin {
         if (prefix.equals("qu")) {
             sb.delete(0, 1);
         }
-
-        sb.append(prefix + "ay");
+        
+        /*
+        sb.replace(0, 1, sb.toString().substring(0, 1).toUpperCase());
+        prefix = prefix.toLowerCase();
+        */
+        sb.append(prefix + "ay" + endPunctuation.toString());
 
         return sb.toString();
     }
